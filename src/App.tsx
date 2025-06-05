cf5a8d92dd241064d89056640044c692788f2ac7
islamsedibrahim12345 <dr.sedeek.toolan@alexu.edu.eg>
2025-06-05 22:02:20 +0300

    Deploy application to Netlify

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1>Family Health Platform</h1>
+    </div>
+  )
+}
+
+export default App
